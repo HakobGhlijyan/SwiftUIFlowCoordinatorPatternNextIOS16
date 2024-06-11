2473726e134879c5b214d835540e31f93809065c
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-06-11 15:33:40 +0400

    Add

--- SwiftUIFlowCoordinatorPatternNextIOS16/SwiftUIFlowCoordinatorPatternNextIOS16App.swift
+++ SwiftUIFlowCoordinatorPatternNextIOS16/SwiftUIFlowCoordinatorPatternNextIOS16App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIFlowCoordinatorPatternNextIOS16App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: ContentViewModel())
         }
     }
 }
