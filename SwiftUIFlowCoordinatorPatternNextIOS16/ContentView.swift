952a91a798f269dc37371dad8a8dbd60748cbfec
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-06-11 15:22:48 +0400

    Initial Commit

---
+++ SwiftUIFlowCoordinatorPatternNextIOS16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIFlowCoordinatorPatternNextIOS16
+//
+//  Created by Hakob Ghlijyan on 11.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIFlowCoordinatorPatternNextIOS16/SwiftUIFlowCoordinatorPatternNextIOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFlowCoordinatorPatternNextIOS16App.swift
+//  SwiftUIFlowCoordinatorPatternNextIOS16
+//
+//  Created by Hakob Ghlijyan on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFlowCoordinatorPatternNextIOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
